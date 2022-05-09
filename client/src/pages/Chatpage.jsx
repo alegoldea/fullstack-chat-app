6afb37268081684158fe4a0357d5fd7b6174828a
alegoldea <ale.goldea@gmail.com>
2022-05-09 15:28:24 +0300

    Rendering data in React App

--- client/src/pages/Chatpage.jsx
+++ client/src/pages/Chatpage.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Chatpage = () => {
+  const [chats, setChats] = useState([]);
   const fetchChats = async () => {
-    const data = await axios.get("/api/chat");
-    console.log(data);
+    const { data } = await axios.get("http://localhost:5000/api/chat");
+    setChats(data);
   };
 
   useEffect(() => {
     fetchChats();
   }, []);
-  return <div>Chats</div>;
+
+  return (
+    <div>
+      {chats.map((chat) => (
+        <div key={chat._id}>{chat.chatName}</div>
+      ))}
+      Chat Page
+    </div>
+  );
 };
 
 export default Chatpage;
--- server/index.js
+++ server/index.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const { chats } = require("./data/data");
 
 const app = express();
 dotenv.config();
+
+const server = require("http").createServer(app);
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
 app.get("/api/chat", (req, res) => {
-  res.send("Chats");
+  res.send(chats);
 });
 
 app.get("/api/chat/:id", (req, res) => {
-  //console.log(req.params.id);
-  const singleChat = chats.find((c) => c.id === req.params.id);
+  console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
   res.send(singleChat);
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`Server started on port ${PORT}... `));
+server.listen(5000, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
