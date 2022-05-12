4529ad10fc05e5ca451e7849ea70397d581667ca
alegoldea <ale.goldea@gmail.com>
2022-05-12 22:19:08 +0300

    Created APIs for sending and fetching messages

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
@@ -29,6 +30,7 @@ app.use(express.json());
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
