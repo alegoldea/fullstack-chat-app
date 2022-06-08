98cc864178454ef3ea2e0fc02990b72f31bd19c4
alegoldea <ale.goldea@gmail.com>
2022-06-08 12:28:35 +0300

    Added encodedPublicKey field to backend

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -3,7 +3,7 @@ const User = require("../models/userModel");
 const { generateToken } = require("../config/generateToken");
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password, pic } = req.body;
+  const { name, email, password, pic, encodedPublicKey } = req.body;
 
   if (!name || !email || !password) {
     res.status(400);
@@ -22,6 +22,7 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password,
     pic,
+    encodedPublicKey,
   });
 
   if (user) {
@@ -30,6 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       pic: user.pic,
+      encodedPublicKey: user.encodedPublicKey,
       token: generateToken(user._id),
     });
   } else {
@@ -47,6 +49,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       pic: user.pic,
+      encodedPublicKey: user.encodedPublicKey,
       token: generateToken(user._id),
     });
   } else {
